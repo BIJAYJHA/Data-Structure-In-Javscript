eaf38fdd1443d0c6b13accf4ae4af7db16452a09
BIJAYJHA <bijaykumarjha0209@gmail.com>
2019-12-30 09:35:35 +0530

    deleteing a node and some more extra adding functions in linked list

--- Linked List/LinkedList.js
+++ Linked List/LinkedList.js
@@ -1,37 +1,112 @@
-class LinkedList{
+class LinkedList {
 
-    constructor(){
-        this.head= null;
-        this.size=0;
+    constructor() {
+        this.head = null;
+        this.size = 0;
     }
 
-    addElement(element){
-        let node1;
-         node1 = new node(element);
+    getTheSizeOfTheLinkedList() {
+        console.log("size of the linked list===>", this.size);
+    }
+    addElementAtFirst(element) {
+        let firstNode;
+        firstNode = new node(element);
         let current;
-        if(this.head===null){
-            this.head=node1;
+        if (this.head === null) {
+            this.head = firstNode;
+        } else {
+            current = this.head;
+            this.head = firstNode;
+            firstNode.next = current;
+        }
+        this.size++;
+    }
 
-        }else{
-            current=this.head;
-            while(current.next){
-                current=current.next;
+    addElementAtLast(element) {
+        let node1;
+        node1 = new node(element);
+        let current;
+        if (this.head === null) {
+            this.head = node1;
+
+        } else {
+            current = this.head;
+            while (current.next) {
+                current = current.next;
             }
-            current.next=node1;
+            current.next = node1;
         }
         this.size++;
 
     }
+    deleteAnyElement(element) {
 
-    printList(){
-        let curr=this.head;
-        let str="";
-        while(curr){
-            str+=curr.element+" ";
-            curr=curr.next;
+        let current = this.head;
+        let prev = null;
+
+        // iterate over the list 
+        while (current != null) {
+            // comparing element with current 
+            // element if found then remove the 
+            // and return true 
+            if (current.element === element) {
+                if (prev == null) {
+                    this.head = current.next;
+                } else {
+                    prev.next = current.next;
+                }
+                this.size--;
+                return current.element;
+            }
+            prev = current;
+            current = current.next;
+        }
+        return -1;
+    }
+    addElemntAfterAnElement(element, newElement) {
+        let current = this.head;
+        let prev = null;
+        let newNode = new node(newElement);
+        if (this.head === null) {
+            console.log("you cannot add an element after an element on an empty linked list");
+        } else {
+            while (current != null) {
+                if (current.element === element) {
+                    console.log("current elemnt====>", current.element);
+                    console.log("prev=====>", prev.element);
+                    if (current.next === null) {
+                        current.next = newNode;
+                        newNode.next = null;
+                        break;
+                    } else {
+                        newNode.next = current.next;
+                        current.next = newNode;
+                        break;
+
+                    }
+
+                    //prev.next=current;
+                }
+                this.size++;
+                prev = current;
+                current = current.next;
+
+            }
+
+        }
+
+
+    }
+
+    printList() {
+        let curr = this.head;
+        let str = "";
+        while (curr) {
+            str += curr.element + " ";
+            curr = curr.next;
         }
         console.log(str);
     }
 
-    
+
 }
--- Linked List/main.js
+++ Linked List/main.js
@@ -1,10 +1,21 @@
-class main{
-    constructor(){
-    let ll= new LinkedList();
-    ll.addElement(10);
-    ll.addElement(20);
-    ll.addElement(30);
-    ll.printList();
+class main {
+    constructor() {
+        let ll = new LinkedList();
+        ll.addElementAtLast(10);
+        ll.addElementAtLast(20);
+        ll.addElementAtLast(30);
+        ll.printList();
+        // console.log("deleteding an element");
+        // ll.deleteAnyElement(20);
+        // ll.printList();
+        ll.addElementAtFirst(1);
+        ll.addElementAtFirst(0);
+        ll.printList();
+        ll.getTheSizeOfTheLinkedList();
+        ll.addElemntAfterAnElement(20, 25);
+        ll.printList();
+        ll.addElemntAfterAnElement(30, 35);
+        ll.printList();
     }
 
 }
